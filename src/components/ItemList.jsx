e0568570310d93dbbbeaec6f2fcc5ebe27fae33e
hatim77100 <najoua-hatim@hotmail.fr>
2024-10-08 22:39:51 +0200

    🔨 update state & name props

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -10,8 +10,8 @@ export default function ItemList({
           <Item
             key={item.id}
             item={item}
-            handleDeleteItem={handleDeleteItem}
-            handleToggleItem={handleToggleItem}
+            onDeleteItem={handleDeleteItem}
+            onToggleItem={handleToggleItem}
           />
         );
       })}
@@ -19,18 +19,18 @@ export default function ItemList({
   );
 }
 
-function Item({ item, handleDeleteItem, handleToggleItem }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li className="item">
       <label>
         <input
           type="checkbox"
           checked={item.packed}
-          onChange={() => handleToggleItem(item.id)}
+          onChange={() => onToggleItem(item.id)}
         />
         {item.name}
       </label>
-      <button onClick={() => handleDeleteItem(item.id)}>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
