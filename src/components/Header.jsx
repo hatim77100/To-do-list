8c11c0f01761608f5ed9ba1df589c85337fc8678
hatim77100 <najoua-hatim@hotmail.fr>
2024-10-09 13:03:00 +0200

    🔨 compute derived state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import Counter from "./Counter";
 import Logo from "./Logo";
 
-export default function Header() {
+export default function Header({ totalNumberOfItems, numberOfItemsPacked }) {
   return (
     <header>
       <Logo />
-      <Counter />
+      <Counter
+        numberOfItemsPacked={numberOfItemsPacked}
+        totalNumberOfItems={totalNumberOfItems}
+      />
     </header>
   );
 }
